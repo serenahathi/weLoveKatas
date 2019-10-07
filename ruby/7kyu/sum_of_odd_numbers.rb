8126b49af477669b86e853461182b934a9ddaa4d
Serena Hathi <shathi@babbel.com>
2019-10-07 11:57:40 +0200

    Add sum of odd numbers kata

---
+++ ruby/7kyu/sum_of_odd_numbers.rb
@@ -0,0 +1,8 @@
+def row_sum_odd_numbers(n)
+  return n ** 3
+end
+
+row_sum_odd_numbers(1) # sum = 1
+row_sum_odd_numbers(2) # sum = 8
+row_sum_odd_numbers(3) # sum = 27
+row_sum_odd_numbers(4) # sum = 64
